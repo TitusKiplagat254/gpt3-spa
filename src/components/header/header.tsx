e049391bcefc14685620049987ea8b587526ffc5
Titus Kiplagat <tituskiplagat.skillhub@gmail.com>
2025-06-07 11:57:58 +0300

    Add header component

---
+++ src/components/header/header.tsx
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import logo from "../../assets/logo.svg";
+
+import "./header.css";
+import { RiCloseFill, RiMenu3Fill } from "react-icons/ri";
+
+const Header = () => {
+	const [toggleMenu, setToggleMenu] = useState(false);
+	return (
+		<div className="navbar">
+			<div className="navbar__content">
+				<div className="navbar__logo">
+					<img src={logo} alt="Header logo icon" />
+				</div>
+				<div className="navbar__links">
+					<p>
+						<a href="#home">Home</a>
+					</p>
+					<p>
+						<a href="#wgpt3">What is GPT3?</a>
+					</p>
+					<p>
+						<a href="#possibility">Open AI</a>
+					</p>
+					<p>
+						<a href="#features">Case Studies</a>
+					</p>
+					<p>
+						<a href="#blog">Library</a>
+					</p>
+				</div>
+			</div>
+			<div className="navbar__auth">
+				<p>Sign in</p>
+				<button type="button">Sign up</button>
+			</div>
+
+			<div className="navbar__menu">
+				{toggleMenu ? (
+					<RiCloseFill
+						color="#fff"
+						size={27}
+						onClick={() => setToggleMenu(false)}
+					/>
+				) : (
+					<RiMenu3Fill
+						color="#fff"
+						size={27}
+						onClick={() => setToggleMenu(true)}
+					/>
+				)}
+				{toggleMenu && (
+					<div className="navbar__menu-content">
+						<div className="navbar__menu-links">
+							<p>
+								<a href="#home">Home</a>
+							</p>
+							<p>
+								<a href="#wgpt3">What is GPT3?</a>
+							</p>
+							<p>
+								<a href="#possibility">Open AI</a>
+							</p>
+							<p>
+								<a href="#features">Case Studies</a>
+							</p>
+							<p>
+								<a href="#blog">Library</a>
+							</p>
+						</div>
+						<div className="navbar__menu-auth">
+							<p>Sign in</p>
+							<button type="button">Sign up</button>
+						</div>
+					</div>
+				)}
+			</div>
+		</div>
+	);
+};
+
+export default Header;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
-export { default as Brand } from "./brand.tsx";
-export { default as CallToAction } from "./callToAction.tsx";
-export { default as Footer } from "./footer.tsx";
-export { default as Header } from "./header.tsx";
+export { default as Brand } from "./brand";
+export { default as CallToAction } from "./callToAction";
+export { default as Footer } from "./footer";
+export { default as Header } from "./header/header";
