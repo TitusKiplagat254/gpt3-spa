8c355954f18fb3f362ebd86a2f42b5bc73258893
Titus Kiplagat <tituskiplagat.skillhub@gmail.com>
2025-06-12 14:28:45 +0300

    Add possibility section

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,7 +4,18 @@ import dropbox from "./dropbox.png";
 import google from "./google.png";
 import logo from "./logo.svg";
 import people from "./people.png";
+import possibility from "./possibility.png";
 import shopify from "./shopify.png";
 import slack from "./slack.png";
 
-export { people, ai, logo, atlassian, shopify, slack, google, dropbox };
+export {
+	people,
+	ai,
+	logo,
+	atlassian,
+	shopify,
+	slack,
+	google,
+	dropbox,
+	possibility,
+};
--- src/components/sections/possibility/possibility.tsx
+++ src/components/sections/possibility/possibility.tsx
@@ -1,9 +1,28 @@
+import { possibility } from "../../../assets";
+
+import "./possibility.css";
+
 const Possibility = () => {
-  return (
-    <div>
-      <h1>Possibility</h1>
-    </div>
-  );
+	return (
+		<div id="possibility" className="possibility section__padding">
+			<div className="possibility__image">
+				<img src={possibility} alt="possibility image" />
+			</div>
+			<div className="possibility__content">
+				<h4>Request Early Access to Get Started</h4>
+				<h1 className="gradient__text">
+					The possibilities are <br /> beyond your imagination
+				</h1>
+				<p>
+					Yet bed any for travelling assistance indulgence unpleasing. Not
+					thoughts all exercise blessing. Indulgence way everything joy
+					alteration boisterous the attachment. Party we years to order allow
+					asked of.
+				</p>
+				<h4>Request Early Access to Get Started</h4>
+			</div>
+		</div>
+	);
 };
 
 export default Possibility;
