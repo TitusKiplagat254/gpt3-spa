f35af4f6a6b6c6e62a03528dbb0d88f082435480
Titus Kiplagat <tituskiplagat.skillhub@gmail.com>
2025-06-12 12:48:23 +0300

    Add features section

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -17,7 +17,7 @@ const Header = () => {
 						<a href="#home">Home</a>
 					</p>
 					<p>
-						<a href="#wgpt3">What is GPT3?</a>
+						<a href="#whatIsGPT">What is GPT3?</a>
 					</p>
 					<p>
 						<a href="#possibility">Open AI</a>
@@ -56,7 +56,7 @@ const Header = () => {
 								<a href="#home">Home</a>
 							</p>
 							<p>
-								<a href="#wgpt3">What is GPT3?</a>
+								<a href="#whatIsGPT">What is GPT3?</a>
 							</p>
 							<p>
 								<a href="#possibility">Open AI</a>
--- src/components/sections/features/features.tsx
+++ src/components/sections/features/features.tsx
@@ -1,9 +1,29 @@
+import { featuresData } from "../../../data/features";
+import { Feature } from "../../ui";
+
+import "./features.css";
+
 const Features = () => {
-  return (
-    <div>
-      <h1>Features</h1>
-    </div>
-  );
+	return (
+		<div id="features" className="features section__padding">
+			<div className="features__heading">
+				<h1 className="gradient__text">
+					The Future is Now and You Just Need to Realize It. Step into Future
+					Today. & Make it Happen.
+				</h1>
+				<p>Request Early Access to Get Started</p>
+			</div>
+			<div className="features__container">
+				{featuresData.map((item, index) => (
+					<Feature
+						title={item.title}
+						text={item.text}
+						key={item.title + index}
+					/>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default Features;
