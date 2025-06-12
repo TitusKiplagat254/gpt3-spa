5cb2e111ce5850f6c88e0213bc75705f9afa1545
Titus Kiplagat <tituskiplagat.skillhub@gmail.com>
2025-06-12 16:02:59 +0300

    Add the call-to-action component

---
+++ src/components/callToAction/callToAction.tsx
@@ -0,0 +1,17 @@
+import "./callToAction.css";
+
+const CallToAction = () => {
+	return (
+		<div className="cta">
+			<div className="cta__content">
+				<p>Request Early Access to Get Started</p>
+				<h3>Register Today & start exploring the endless possibilities.</h3>
+			</div>
+			<div className="cta__btn-container">
+				<button type="button">Get Started</button>
+			</div>
+		</div>
+	);
+};
+
+export default CallToAction;
